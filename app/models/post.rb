b98e3785d9d9b32f1b7b5ca2a84025fc6bfdc94a
Carl <carlbrainegebe@gmail.com>
2021-08-19 21:31:08 -0500

    commit tree fde5f2a7647201b5686f8f1066bc6c405ace3c19

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
+    validate :clickbait?
+
+    def clickbait?
+       if [/won't believe/i, /secret/i, /top \d/i, /guess/i].none? { |bait| bait.match title }
+        errors.add(:title, "too much integrity in this title")
+        end
+    end
+
 end
